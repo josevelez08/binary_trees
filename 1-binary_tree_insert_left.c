afcb6bf7ab10ccab9214c644bac933a841a49258
Sergio <2016@holbertonschool.com>
2021-03-11 00:33:07 -0500

    Rebuild the function to fix errors

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,35 +3,26 @@
  * binary_tree_insert_left - inserts a node as the left of another node
  * @parent:  pointer to the node to insert the left-child in
  * @value: value to store in the new node
- * Return:  pointer to the created node 
+ * Return:  pointer to the created node.
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	if (parent->left == NULL)
-	{
-	parent->left = malloc(sizeof(binary_tree_t));
-	parent->left->parent = parent;
-	parent = parent->left;
-	parent->n = value;
-	parent->left = NULL;
-	parent->right = NULL;
-	return (parent);
-	}
-	else
-	{
-	binary_tree_t *ref;
+	binary_tree_t *new_ref = NULL;
 
-	ref = malloc(sizeof(binary_tree_t));
-	if(ref == NULL)
+	if (!parent)
+		return (NULL);
+
+	new_ref = binary_tree_node(parent, value);
+	if (!new_ref)
+		return (NULL);
+
+	if (parent->left)
 	{
-		return NULL;
+		parent->left->parent = new_ref;
+		new_ref->left = parent->left;
 	}
-	ref->parent = parent;
-	ref->right = NULL;
-	ref->n = value;
-	parent->left->parent = ref;
-	ref->left = parent->left;
-	parent->left = ref;
-	return (parent);
-	}
+
+	parent->left = new_ref;
+	return (new_ref);
 }
